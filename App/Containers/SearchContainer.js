2472ca1615d09e20c22b71f3042773990d0118ff
LouisLoiseau <louis.loiseau-billon@supinternet.fr>
2019-05-25 17:46:21 +0200

    Added base sections list

--- App/Containers/SearchContainer.js
+++ App/Containers/SearchContainer.js
@@ -1,29 +1,65 @@
 import React, { Component } from "react";
-import { View, Text, Keyboard } from "react-native";
+import { View, Text, SectionList, TouchableOpacity } from "react-native";
 import { History } from "../Lib";
 import { AppStyles } from "../Themes";
 import { SearchBar } from "../Components";
 import Styles from "./Styles/SearchContainerStyles";
 import Api from "../Api";
+import FontAwesome, { Icons } from "react-native-fontawesome";
 
 class SearchContainer extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
 			query: "",
+			searchHistoryList: [],
 		};
 		this.input = null;
 		this.search = this.search.bind(this);
-	}
-
-	search() {
-		let { query } = this.state;
-		Api.search(query).then(response => {
-			console.log(response);
-		});
+		this.renderItem = this.renderItem.bind(this);
 	}
 
 	componentDidMount() {
+		this.setState({
+			searchHistoryList: [
+				{
+					title: "Dernières recherches", data: [
+						{ text: "lloyd price" },
+						{ text: "pnb rock" },
+						{ text: "Masterpiece" },
+						{ text: "i'm gonna knock on your door" },
+						{ text: "i'm gonna knock" },
+					],
+				},
+				{
+					title: "Les plus recherchés", data: [
+						{ text: "jonas brothers" },
+						{ text: "dj khaled" },
+						{ text: "angèle" },
+						{ text: "old town road" },
+						{ text: "tabata mixtape, vol1. 1" },
+					],
+				},
+				{
+					title: "Les plus recherchés", data: [
+						{ text: "jonas brothers" },
+						{ text: "dj khaled" },
+						{ text: "angèle" },
+						{ text: "old town road" },
+						{ text: "tabata mixtape, vol1. 1" },
+					],
+				},
+				{
+					title: "Les plus recherchés", data: [
+						{ text: "jonas brothers" },
+						{ text: "dj khaled" },
+						{ text: "angèle" },
+						{ text: "old town road" },
+						{ text: "tabata mixtape, vol1. 1" },
+					],
+				},
+			]
+		});
 		this.focusListener = this.props.navigation.addListener("didFocus", () => {
 			if (this.input !== null && this.input !== undefined) {
 				this.input.focus();
@@ -35,6 +71,28 @@ class SearchContainer extends Component {
 		this.focusListener.remove();
 	}
 
+	search() {
+		let { query } = this.state;
+		Api.search(query).then(response => {
+			console.log(response);
+		});
+	}
+
+	renderSectionHeader({ section }) {
+		return (
+			<Text style={Styles.sectionHeaderTitle}>{section.title}</Text>
+		)
+	}
+
+	renderItem({ item, index }) {
+		return (
+			<TouchableOpacity onPress={() => this.search(item.text)} style={Styles.itemContainer} hitSlop={{ top: 10, bottom: 10, left: 10, right: 10 }}>
+				<FontAwesome>{Icons.clock}</FontAwesome>
+				<Text style={Styles.itemText}>{item.text}</Text>
+			</TouchableOpacity>
+		)
+	}
+
 	render() {
 		return (
 			<View style={[AppStyles.container, Styles.container]}>
@@ -42,6 +100,13 @@ class SearchContainer extends Component {
 					setRef={ref => this.input = ref}
 					onChangeText={text => this.setState({ query: text.split(" ").join("+") })}
 					onSubmitEditing={this.search}
+				/>
+				<SectionList
+					sections={this.state.searchHistoryList}
+					renderSectionHeader={this.renderSectionHeader}
+					renderItem={this.renderItem}
+					style={Styles.sectionList}
+					contentContainerStyle={Styles.sectionListContent}
 				/>
 			</View>
 		);
--- App/Containers/Styles/SearchContainerStyles.js
+++ App/Containers/Styles/SearchContainerStyles.js
@@ -4,5 +4,33 @@ import { Metrics, AppStyles } from "../../Themes";
 export default StyleSheet.create({
   container: {
     paddingTop: Metrics.headerHeight,
+    paddingHorizontal: 0,
+  },
+
+  sectionHeaderTitle: {
+    fontSize: 20,
+    color: "#000",
+    fontWeight: "bold",
+    marginBottom: 5,
+  },
+
+  itemContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "flex-start",
+    marginVertical: 14,
+  },
+
+  itemText: {
+    marginLeft: 15,
+  },
+
+  sectionList: {
+    backgroundColor: "#fafafa",
+  },
+
+  sectionListContent: {
+    paddingHorizontal: Metrics.paddingHorizontal,
+    paddingVertical: Metrics.statusBarHeight / 2,
   },
 });
